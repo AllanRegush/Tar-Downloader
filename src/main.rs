e31c46544844ba8e93cfbe7e226c9bb447dc4317
Allan Regush <17693494+AllanRegush@users.noreply.github.com>
2022-03-15 10:36:11 -0600

    Config: Strip release binary

--- src/main.rs
+++ src/main.rs
@@ -8,9 +8,10 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args: Vec<String> = env::args().collect();
     let folder = &args[1];
     let url = &args[2];
-    let file_extension = &url[url.len() - 3 ..url.len()];
-    if !file_extension.eq(".gz") {
-        return Err("url request must be a .gz file".into())
+    let file_extension_gz = &url[url.len() - 3 ..url.len()];
+    let file_extension_tgz = &url[url.len() - 4 ..url.len()];
+    if !file_extension_gz.eq(".gz") || !file_extension_tgz.eq(".tgz") {
+        return Err("url request must be a .gz or .tgz file".into())
     }
     let response = reqwest::blocking::get(url)?;
     let content =  Cursor::new(response.bytes().unwrap());
