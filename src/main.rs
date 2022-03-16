b9f1b76ff814c8139f97441061248bfcedd2ce3f
Allan Regush <17693494+AllanRegush@users.noreply.github.com>
2022-03-15 18:05:14 -0600

    Feat: Add help and args validation

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,28 @@
-
-use std::io::Cursor;
-use std::env;
+use clap::{arg, command};
 use flate2::read::GzDecoder;
+use std::env;
+use std::io::Cursor;
 use tar::Archive;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let args: Vec<String> = env::args().collect();
-    let folder = &args[1];
-    let url = &args[2];
-    let file_extension_gz = &url[url.len() - 3 ..url.len()];
-    let file_extension_tgz = &url[url.len() - 4 ..url.len()];
-    println!("{:?}", file_extension_tgz);
+    let matches = command!()
+        .arg(arg!([folder] "Folder Name of where the Archive will be extracted").required(true))
+        .arg(arg!([url] "URL of the file to be downloaded").required(true))
+        .get_matches();
+
+    let folder = matches.value_of("folder").unwrap();
+    let url = matches.value_of("url").unwrap();
+    let file_extension_gz = &url[url.len() - 3..url.len()];
+    let file_extension_tgz = &url[url.len() - 4..url.len()];
     if !file_extension_gz.eq(".gz") && !file_extension_tgz.eq(".tgz") {
-        return Err("url request must be a .gz or .tgz file".into())
+        return Err("url request must be a .gz or .tgz file".into());
     }
     let response = reqwest::blocking::get(url)?;
-    let content =  Cursor::new(response.bytes().unwrap());
+    let content = Cursor::new(response.bytes().unwrap());
     let tar = GzDecoder::new(content);
     let mut archive = Archive::new(tar);
     archive.unpack(format!("{}", folder))?;
+
     Ok(())
 }
+
